620f93e337c312766aaf095a8a2d1b01d562d415
Sayan De <sayandeten@gmail.com>
2025-12-31 18:03:08 +0530

    feat: Add ShootingStars and StarsBackground components with a new layout and title to the chat page.

--- apps/web/app/(portals)/chat/page.tsx
+++ apps/web/app/(portals)/chat/page.tsx
@@ -1,5 +1,18 @@
+import { ShootingStars } from "@workspace/ui/components/shooting-stars";
+import { StarsBackground } from "@workspace/ui/components/stars-background";
 import React from "react";
 
 export default function ChatPage() {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="rounded-md bg-neutral-900 flex flex-col items-center justify-center relative w-full h-screen">
+        <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
+          <span>Shooting Star</span>
+          <span>Star Background</span>
+        </h2>
+        <ShootingStars />
+        <StarsBackground />
+      </div>
+    </div>
+  );
 }
